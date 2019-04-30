7d255bb53e1748d7792ba9d5aa65f80fb8e59db7
Steven Hyland <2knowindeed@gmail.com>
2019-04-29 23:15:06 -0400

    that was harder than I expected

---
+++ 2015/p8.py
@@ -0,0 +1,22 @@
+import aoc
+
+input = aoc.getInput(8)
+
+
+def getMemoryLength(line):
+    newline = line.encode("utf-8").decode("unicode-escape")
+    return len(newline) - 2
+
+
+def getEncodedLength(line):
+    newline = '"' + line.replace('\\', '\\\\').replace('"', '\\"') + '"'
+    print(line, newline, len(line), len(newline))
+    return len(newline)
+
+
+total = 0
+for line in input.splitlines():
+    #total += len(line) - getMemoryLength(line)
+    total += getEncodedLength(line) - len(line)
+
+print(total)
