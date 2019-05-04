3b859a7067450cd61569ed321faff42a5dab290b
Steven Hyland <2knowindeed@gmail.com>
2019-05-04 16:19:29 -0400

    incrementing stringz

--- 2015/p11.py
+++ 2015/p11.py
@@ -11,18 +11,43 @@ def ascii_increment(codes, i):
         ascii_increment(codes, i - 1)
 
 
-def next_password(pw):
-    codes = list(map(lambda x: ord(x), pw))
+def next_password(password):
+    codes = list(map(lambda x: ord(x), password))
     ascii_increment(codes, -1)
     return ''.join(list(map(lambda x: chr(x), codes)))
 
 
-def req_one
+def req_one(password):
+    for i in range(len(password) - 2):
+        if ord(password[i]) + 1 == ord(password[i+1]) and ord(password[i+1]) + 1 == ord(password[i+2]):
+            return True
+    return False
+
+
+def req_two(password):
+    return 'i' not in password and 'o' not in password and 'l' not in password
+
+
+def req_three(password):
+    firstFind = ''
+    for i in range(len(password) - 1):
+        if password[i] == password[i+1]:
+            if firstFind == '':
+                firstFind = password[i]
+            elif firstFind != password[i]:
+                return True
+    return False
 
 
 input = aoc.get_input(11)
 
 print(input)
-for i in range(51):
+while not req_one(input) or not req_two(input) or not req_three(input):
+    input = next_password(input)
+print(input)
+
+input = next_password(input)
+
+while not req_one(input) or not req_two(input) or not req_three(input):
     input = next_password(input)
 print(input)
