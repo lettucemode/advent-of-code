939b0f5e40cc3b6c1069a0bb7871d2d02ba5aec4
Steven Hyland <2knowindeed@gmail.com>
2019-05-15 21:31:09 -0400

    combinations are hard

--- 2015/p17.py
+++ 2015/p17.py
@@ -1,5 +1,22 @@
 import aoc
+import itertools
 
 input = aoc.get_input(17)
+containers = []
+combinations = 0
+count = 0
 
-print(input)
+for line in input.splitlines():
+    containers.append(int(line))
+
+lengths = [0 for i in range(len(containers))]
+
+for i in range(len(containers)):
+    for combo in itertools.combinations(containers, i):
+        if (sum(combo)) == 150:
+            print(combo)
+            lengths[i] += 1
+            count += 1
+
+print(count)
+print(lengths)
