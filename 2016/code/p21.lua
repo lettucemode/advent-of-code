c44f40dfcb8808952a6927f86b74a6d280f7a0fd
Steven Hyland <2knowindeed@gmail.com>
2019-08-28 23:10:07 -0400

    done with 21 and 22!

--- 2016/code/p21.lua
+++ 2016/code/p21.lua
@@ -2,31 +2,74 @@ local hylib = require 'hylib'
 
 io.input('../inputs/p21.txt')
 local input = io.read('*a')
-local password = 'abcdefgh'
 
-local instructions = hylib.iter_to_table(input:splitlines())
-for i=1,#instructions do
-    local words = hylib.iter_to_table(instructions[i]:gmatch('%w+'))
-    if words[1] == 'swap' then
-        if words[2] == 'letter' then
-            password = password:swap(words[3], words[6])
-        else
-            password = password:swap(tonumber(words[3])+1, tonumber(words[6])+1)
+function scramble(pw)
+    local instructions = hylib.iter_to_table(input:splitlines())
+    for i=1,#instructions do
+        local words = hylib.iter_to_table(instructions[i]:gmatch('%w+'))
+        if words[1] == 'swap' then
+            if words[2] == 'letter' then
+                pw = pw:swap(words[3], words[6])
+            else
+                pw = pw:swap(tonumber(words[3])+1, tonumber(words[6])+1)
+            end
+        elseif words[1] == 'rotate' then
+            if words[2] == 'based' then
+                local idx = pw:find(words[7])
+                if idx >= 5 then idx = idx + 1 end
+                pw = pw:rotate(idx)
+            else
+                local val = tonumber(words[3])
+                if words[2] == 'left' then val = val * -1 end
+                pw = pw:rotate(val)
+            end
+        elseif words[1] == 'reverse' then
+            pw = pw:reverse_sub(tonumber(words[3])+1, tonumber(words[5])+1)
+        elseif words[1] == 'move' then
+            pw = pw:move(tonumber(words[3])+1, tonumber(words[6])+1)
         end
-    elseif words[1] == 'rotate' then
-        if words[2] == 'based' then
-            local idx = password:find(words[7])
-            if idx >= 5 then idx = idx + 1 end
-            password = password:rotate(idx)
-        else
-            local val = tonumber(words[3])
-            if words[2] == 'left' then val = val * -1 end
-            password = password:rotate(val)
-        end
-    elseif words[1] == 'reverse' then
-        password = password:reverse_sub(tonumber(words[3])+1, tonumber(words[5])+1)
-    elseif words[1] == 'move' then
-        password = password:move(tonumber(words[3])+1, tonumber(words[6])+1)
     end
-    print(password)
+    return pw
 end
+
+
+function unscramble(pw)
+    local instructions = hylib.iter_to_table(input:splitlines())
+    for i=#instructions,1,-1 do
+        local words = hylib.iter_to_table(instructions[i]:gmatch('%w+'))
+        if words[1] == 'swap' then
+            if words[2] == 'letter' then
+                pw = pw:swap(words[3], words[6])
+            else
+                pw = pw:swap(tonumber(words[3])+1, tonumber(words[6])+1)
+            end
+        elseif words[1] == 'rotate' then
+            if words[2] == 'based' then
+                local idx = pw:find(words[7]) - 1
+                if idx == 0 then idx = 7
+                elseif idx == 1 then idx = 7
+                elseif idx == 2 then idx = 2
+                elseif idx == 3 then idx = 6
+                elseif idx == 4 then idx = 1
+                elseif idx == 5 then idx = 5
+                elseif idx == 6 then idx = 0
+                elseif idx == 7 then idx = 4
+                end
+                pw = pw:rotate(idx)
+            else
+                local val = tonumber(words[3])
+                if words[2] == 'right' then val = val * -1 end
+                pw = pw:rotate(val)
+            end
+        elseif words[1] == 'reverse' then
+            pw = pw:reverse_sub(tonumber(words[3])+1, tonumber(words[5])+1)
+        elseif words[1] == 'move' then
+            pw = pw:move(tonumber(words[6])+1, tonumber(words[3])+1)
+        end
+    end
+    return pw
+end
+
+print(scramble('abcdefgh'))
+print ('------')
+print(unscramble('fbgdceah'))
