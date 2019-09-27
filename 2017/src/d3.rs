6e5992b420debe41c4a4f0dac949171a91adf58b
Steven Hyland <2knowindeed@gmail.com>
2019-09-27 10:48:25 -0400

    gridz

--- 2017/src/d3.rs
+++ 2017/src/d3.rs
@@ -4,10 +4,10 @@ use std::cmp;
 pub fn solve() -> (u32, i32, i32) {
     let input = common::get_input("./input/d3.txt");
     let input: i32 = input.parse().unwrap();
-    (3, part_one(&input), part_two(&input))
+    (3, part_one(input), part_two(input))
 }
 
-fn part_one(input: &i32) -> i32 {
+fn part_one(input: i32) -> i32 {
     let mut ring_no: i32 = 0;
     let mut side_length: i32 = 1;
     let mut progress: i32 = 0;
@@ -16,7 +16,7 @@ fn part_one(input: &i32) -> i32 {
     loop {
         ring_size = cmp::max(1, side_length * 4 - 4);
         progress += ring_size;
-        if input < &progress {
+        if input < progress {
             break;
         };
         ring_no += 1;
@@ -41,12 +41,38 @@ fn part_one(input: &i32) -> i32 {
     ring_no + dist_from_sweet_spot
 }
 
-fn part_two(input: &i32) -> i32 {
+fn part_two(input: i32) -> i32 {
+    // setup
+    let input = input as u32;
     let mut grid = [[0u32; 500]; 500];
     grid[249][249] = 1;
+    let directions: [Direction; 4] = [
+        Direction::Right,
+        Direction::Up,
+        Direction::Left,
+        Direction::Down,
+    ];
+    let mut dir = 0;
+
+    // lez go
     let mut location = Point { x: 250, y: 249 };
-    let (nsum, nnum) = sum_and_num_neighbors(&grid, location);
-    *input
+    let (mut nsum, mut nnum) = sum_and_num_neighbors(&grid, &location);
+    while nsum < input {
+        grid[location.x][location.y] = nsum;
+        if nnum <= 2 {
+            dir = (dir + 1) % directions.len();
+        }
+        match directions[dir] {
+            Direction::Up => location.y = location.y + 1,
+            Direction::Left => location.x = location.x - 1,
+            Direction::Down => location.y = location.y - 1,
+            Direction::Right => location.x = location.x + 1,
+        }
+        let ninfo = sum_and_num_neighbors(&grid, &location);
+        nsum = ninfo.0;
+        nnum = ninfo.1;
+    }
+    nsum as i32
 }
 
 struct Point {
@@ -54,7 +80,7 @@ struct Point {
     y: usize,
 }
 
-fn sum_and_num_neighbors(grid: &[[u32; 500]; 500], point: Point) -> (u32, u32) {
+fn sum_and_num_neighbors(grid: &[[u32; 500]; 500], point: &Point) -> (u32, u32) {
     let mut total: u32 = 0;
     let mut num: u32 = 0;
     for i in (point.x - 1)..=(point.x + 1) {
@@ -66,4 +92,11 @@ fn sum_and_num_neighbors(grid: &[[u32; 500]; 500], point: Point) -> (u32, u32) {
         }
     }
     (total, num)
+}
+
+enum Direction {
+    Up,
+    Left,
+    Down,
+    Right,
 }
---
+++ 2017/src/d4.rs
@@ -0,0 +1,16 @@
+use super::common;
+
+pub fn solve() -> (u32, i32, i32) {
+    let input = common::get_input("./input/d4.txt");
+    let input: i32 = input.parse().unwrap();
+    (4, part_one(input), part_two(input))
+}
+
+fn part_one(input: i32) -> i32 {
+    input
+}
+
+fn part_two(input: i32) -> i32 {
+    input
+}
+
