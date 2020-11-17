1a4aa3e39a3e727683c23d7dc30caa3a48f5f448
Steven Hyland <2knowindeed@gmail.com>
2020-11-16 20:46:33 -0500

    that was a doozy. my solution is probably pretty ass, but it's done

--- 2017/src/d21.rs
+++ 2017/src/d21.rs
@@ -1,33 +1,94 @@
 use std::collections::HashMap;
-use std::mem;
 
 const ON: char = '#';
 const OFF: char = '.';
 const P1_ITERATIONS: u32 = 5;
+const P2_ITERATIONS: u32 = 18;
 
 pub fn solve(input: String) -> (u32, u32) {
-    let pattern: Vec<Vec<char>> = starting_pattern();
+    let mut grid = String::from(".#./..#/###");
     let rules: HashMap<String, String> = build_rules(input);
-    let pattern_str = grid_to_str(&pattern);
-    let next_pattern = str_to_grid(&rules[&pattern_str]);
-    for _ in 0..P1_ITERATIONS {}
-    (pixels_on(&next_pattern), next_pattern.len() as u32)
+    for _ in 0..P1_ITERATIONS {
+        grid = iterate(&grid, &rules);
+    }
+    let p1_ans = pixels_on(&grid);
+    for _ in 0..P2_ITERATIONS - P1_ITERATIONS {
+        grid = iterate(&grid, &rules);
+    }
+    (p1_ans, pixels_on(&grid))
 }
 
-fn next_pattern(grid: Vec<Vec<char>>, rules: &HashMap<String, String>) -> Vec<Vec<char>> {
-    let mut sub_length: usize = 3;
-    if grid.len() < 4 {
-        let grid_str = grid_to_str(&grid);
-        return str_to_grid(&rules[&grid_str]);
-    } else if grid.len() % 2 == 0 {
-        sub_length = 2;
+fn iterate(grid: &str, rules: &HashMap<String, String>) -> String {
+    let num_slashes = grid.chars().filter(|&c| c == '/').count();
+    let row_size = (((grid.len() - num_slashes) % 2 != 0) as u32 + 2) as usize;
+
+    // split grid into array of strings - one for each section
+    let mut patterns: Vec<String> = vec![];
+    let mut builds: Vec<Vec<char>> = vec![vec![]];
+    let mut str_count = 0;
+    let mut ch_count = 0;
+    for ch in grid.chars() {
+        if ch == '/' {
+            ch_count = 0;
+            str_count = 0;
+        } else {
+            builds[str_count].push(ch);
+            ch_count += 1;
+            if ch_count == row_size {
+                if (row_size == 2 && builds[str_count].len() == 5)
+                    || (row_size == 3 && builds[str_count].len() == 11)
+                {
+                    patterns.push(builds[str_count].clone().into_iter().collect());
+                    builds[str_count].clear();
+                } else {
+                    builds[str_count].push('/');
+                }
+                str_count += 1;
+                if str_count == builds.len() {
+                    builds.push(vec![]);
+                }
+                ch_count = 0;
+            }
+        }
     }
 
-    let mut rows_iter = grid.chunks_exact(sub_length);
-    for rowset in rows_iter {}
-    for tlc in 0..grid.len() / sub_length {}
+    // transform each section
+    for i in 0..patterns.len() {
+        patterns[i] = rules[&patterns[i]].clone();
+    }
 
-    vec![vec![]]
+    let row_len = patterns[0].find('/').unwrap();
+
+    // put them back together
+    if patterns.len() == 1 {
+        return patterns[0].clone();
+    } else {
+        let combo_limit = (patterns.len() as f64).sqrt() as usize;
+        let mut result = String::from("");
+
+        let mut row = 0;
+        let mut p = 0;
+        let mut c = 0;
+        loop {
+            let chunk = patterns[p].split_terminator('/').collect::<Vec<&str>>()[c];
+            result.push_str(chunk);
+            p += 1;
+            if p % combo_limit == 0 {
+                p = row;
+                c += 1;
+                if c == row_len {
+                    row += combo_limit;
+                    if row == patterns.len() {
+                        break;
+                    }
+                    p = row;
+                    c = 0;
+                }
+                result.push('/');
+            }
+        }
+        result
+    }
 }
 
 fn build_rules(input: String) -> HashMap<String, String> {
@@ -35,98 +96,85 @@ fn build_rules(input: String) -> HashMap<String, String> {
     for line in input.lines() {
         // insert orig rule
         let things: Vec<&str> = line.split_terminator(" => ").collect();
-        let rule = things[0];
+        let mut input = things[0].to_owned();
         let output = things[1];
-        rules.insert(rule.to_owned(), output.to_owned());
+        rules.insert(input.to_owned(), output.to_owned());
 
         // insert rotations
-        let mut temp_grid = str_to_grid(rule);
-        rotate_cw(&mut temp_grid);
-        let rule = grid_to_str(&temp_grid);
-        rules.insert(rule.to_owned(), output.to_owned());
-        rotate_cw(&mut temp_grid);
-        let rule = grid_to_str(&temp_grid);
-        rules.insert(rule.to_owned(), output.to_owned());
-        rotate_cw(&mut temp_grid);
-        let rule = grid_to_str(&temp_grid);
-        rules.insert(rule.to_owned(), output.to_owned());
+        input = rotate(&input);
+        rules.insert(input.to_owned(), output.to_owned());
+        input = rotate(&input);
+        rules.insert(input.to_owned(), output.to_owned());
+        input = rotate(&input);
+        rules.insert(input.to_owned(), output.to_owned());
 
         // flip it and insert rotations
-        flip_vert(&mut temp_grid);
-        let rule = grid_to_str(&temp_grid);
-        rules.insert(rule.to_owned(), output.to_owned());
-        rotate_cw(&mut temp_grid);
-        let rule = grid_to_str(&temp_grid);
-        rules.insert(rule.to_owned(), output.to_owned());
-        rotate_cw(&mut temp_grid);
-        let rule = grid_to_str(&temp_grid);
-        rules.insert(rule.to_owned(), output.to_owned());
-        rotate_cw(&mut temp_grid);
-        let rule = grid_to_str(&temp_grid);
-        rules.insert(rule.to_owned(), output.to_owned());
+        input = flip(&input);
+        rules.insert(input.to_owned(), output.to_owned());
+        input = rotate(&input);
+        rules.insert(input.to_owned(), output.to_owned());
+        input = rotate(&input);
+        rules.insert(input.to_owned(), output.to_owned());
+        input = rotate(&input);
+        rules.insert(input.to_owned(), output.to_owned());
     }
     rules
 }
 
-fn pixels_on(grid: &Vec<Vec<char>>) -> u32 {
-    grid.iter()
-        .map(|r| r.iter().filter(|c| **c == ON).count() as u32)
-        .sum()
+fn pixels_on(pattern: &str) -> u32 {
+    pattern.chars().filter(|&c| c == ON).count() as u32
 }
 
-fn rotate_cw(grid: &mut Vec<Vec<char>>) {
-    grid.reverse();
-    for i in 1..grid.len() {
-        let (left, right) = grid.split_at_mut(i);
-        for (j, left_item) in left.iter_mut().enumerate().take(i) {
-            mem::swap(&mut left_item[i], &mut right[0][j]);
-        }
+fn rotate(pattern: &str) -> String {
+    let mut rbytes: Vec<u8> = vec![];
+    let pattern_bytes = pattern.as_bytes();
+    if pattern.len() == 5 {
+        rbytes.push(pattern_bytes[3]);
+        rbytes.push(pattern_bytes[0]);
+        rbytes.push(pattern_bytes[2]);
+        rbytes.push(pattern_bytes[4]);
+        rbytes.push(pattern_bytes[1]);
+    } else if pattern.len() == 11 {
+        rbytes.push(pattern_bytes[8]);
+        rbytes.push(pattern_bytes[4]);
+        rbytes.push(pattern_bytes[0]);
+        rbytes.push(pattern_bytes[3]);
+        rbytes.push(pattern_bytes[9]);
+        rbytes.push(pattern_bytes[5]);
+        rbytes.push(pattern_bytes[1]);
+        rbytes.push(pattern_bytes[7]);
+        rbytes.push(pattern_bytes[10]);
+        rbytes.push(pattern_bytes[6]);
+        rbytes.push(pattern_bytes[2]);
+    } else {
+        panic!("called rotate() with a bad-length'd string.");
     }
+    String::from_utf8(rbytes).unwrap()
 }
 
-fn rotate_ccw() {}
-
-fn flip_horiz(grid: &mut Vec<Vec<char>>) {
-    for row in grid.iter() {
-        // row.reverse();
+fn flip(pattern: &str) -> String {
+    let mut rbytes: Vec<u8> = vec![];
+    let pattern_bytes = pattern.as_bytes();
+    if pattern.len() == 5 {
+        rbytes.push(pattern_bytes[3]);
+        rbytes.push(pattern_bytes[4]);
+        rbytes.push(pattern_bytes[2]);
+        rbytes.push(pattern_bytes[0]);
+        rbytes.push(pattern_bytes[1]);
+    } else if pattern.len() == 11 {
+        rbytes.push(pattern_bytes[8]);
+        rbytes.push(pattern_bytes[9]);
+        rbytes.push(pattern_bytes[10]);
+        rbytes.push(pattern_bytes[3]);
+        rbytes.push(pattern_bytes[4]);
+        rbytes.push(pattern_bytes[5]);
+        rbytes.push(pattern_bytes[6]);
+        rbytes.push(pattern_bytes[7]);
+        rbytes.push(pattern_bytes[0]);
+        rbytes.push(pattern_bytes[1]);
+        rbytes.push(pattern_bytes[2]);
+    } else {
+        panic!("called flip() with a bad-length'd string.");
     }
-}
-
-fn flip_vert(grid: &mut Vec<Vec<char>>) {
-    grid.reverse();
-}
-
-fn str_to_grid(pattern: &str) -> Vec<Vec<char>> {
-    let mut ret: Vec<Vec<char>> = vec![vec![]];
-    let mut line: usize = 0;
-    for c in pattern.chars() {
-        match c {
-            ON | OFF => ret[line].push(c),
-            '/' => {
-                ret.push(vec![]);
-                line += 1;
-            }
-            _ => panic!("Unexpected character!"),
-        }
-    }
-    ret
-}
-
-fn grid_to_str(pattern: &Vec<Vec<char>>) -> String {
-    let mut ret = String::new();
-    let mut first_line = true;
-    for line in pattern {
-        if !first_line {
-            ret.push('/');
-        }
-        for c in line {
-            ret.push(*c);
-        }
-        first_line = false;
-    }
-    ret
-}
-
-fn starting_pattern() -> Vec<Vec<char>> {
-    vec![vec![OFF, ON, OFF], vec![OFF, OFF, ON], vec![ON, ON, ON]]
+    String::from_utf8(rbytes).unwrap()
 }
