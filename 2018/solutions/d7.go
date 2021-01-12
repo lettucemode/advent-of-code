181e9404a9efad4f7fecaf0567e468c9d768c8ae
Steven Hyland <2knowindeed@gmail.com>
2021-01-11 23:21:12 -0500

    even more pointy than before. the solution to part 2 was easier than I thought

--- 2018/solutions/d7.go
+++ 2018/solutions/d7.go
@@ -5,21 +5,92 @@ import (
 	"fmt"
 	"io"
 	"regexp"
+	"sort"
 )
 
 // D7Solve ...
 func D7Solve(input io.Reader) (p1 interface{}, p2 interface{}) {
-	nodes := make(map[string]*node)
-	re := regexp.MustCompile(`([A-Z]) must be finished before step ([A-Z])`)
+	lines := make([]string, 0)
 	scanner := bufio.NewScanner(input)
 	for scanner.Scan() {
-		matches := re.FindStringSubmatch(scanner.Text())
-		pn, cn := getNode(&nodes, matches[1]), getNode(&nodes, matches[2])
+		lines = append(lines, scanner.Text())
+	}
+	
+	p1 = partOne(lines)
+	p2 = partTwo(lines)
+	return
+}
+
+func partTwo(lines []string) int {
+
+	nodes := make(map[rune]*node)
+	re := regexp.MustCompile(`([A-Z]) must be finished before step ([A-Z])`)
+	for _, line := range lines {
+		matches := re.FindStringSubmatch(line)
+		pn, cn := getNode(&nodes, toRune(matches[1])), getNode(&nodes, toRune(matches[2]))
 		pn.child = append(pn.child, cn)
 		cn.parent = append(cn.parent, pn)
 	}
-	
-	p1 = ""
+
+	totalTime := 0
+	for len(nodes) > 0 {
+
+		// find available
+		avail := make([]*node, 0)
+		for _, n := range nodes {
+			if len(n.parent) == 0 {
+				avail = append(avail, n)
+			}
+		}
+
+		// if none available, we're done
+		if len(avail) == 0 {
+			break
+		}
+
+		// sort em
+		keys := make([]int, 0)
+		for _, n := range avail {
+			keys = append(keys, int(n.key))
+		}
+		sort.Ints(keys)
+
+		// decrement up to the first five
+		for i := 0; i < 5 && i < len(keys); i++ {
+			n := nodes[rune(keys[i])]
+			n.duration--
+
+			// if duration is now 0, remove it
+			if n.duration == 0 {
+				for _, c := range n.child {
+					for j, p := range c.parent {
+						if p == n {
+							c.parent[j] = c.parent[len(c.parent) - 1]
+							c.parent = c.parent[:len(c.parent) - 1]
+							break
+						}
+					}
+				}
+				delete(nodes, n.key)
+			}
+		}
+		totalTime++
+	}
+	return totalTime
+}
+
+func partOne(lines []string) string {
+
+	nodes := make(map[rune]*node)
+	re := regexp.MustCompile(`([A-Z]) must be finished before step ([A-Z])`)
+	for _, line := range lines {
+		matches := re.FindStringSubmatch(line)
+		pn, cn := getNode(&nodes, toRune(matches[1])), getNode(&nodes, toRune(matches[2]))
+		pn.child = append(pn.child, cn)
+		cn.parent = append(cn.parent, pn)
+	}
+
+	ret := ""
 	for len(nodes) > 0 {
 
 		// find available
@@ -37,7 +108,7 @@ func D7Solve(input io.Reader) (p1 interface{}, p2 interface{}) {
 
 		// get the first alphabetically
 		var next *node
-		minKey := "Z"
+		minKey := 'Z'
 		for _, n := range avail {
 			if n.key <= minKey {
 				next = n
@@ -56,17 +127,20 @@ func D7Solve(input io.Reader) (p1 interface{}, p2 interface{}) {
 			}
 		}
 		delete(nodes, next.key)
-		p1 = fmt.Sprintf("%v", p1) + next.key
+		ret = fmt.Sprintf("%v", ret) + string(next.key)
 	}
-
-	// todo: part 2
-	steps := fmt.Sprintf("%v", p1)
-	p2 = 37
-
-	return
+	return ret
 }
 
-func getNode(nodes *map[string]*node, key string) *node { 
+func toRune(in string) rune {
+	tmp := []rune(in)
+	if len(tmp) > 1 {
+		panic("length of in param greater than one!!!")
+	}
+	return tmp[0]
+}
+
+func getNode(nodes *map[rune]*node, key rune) *node { 
 	n, ok := (*nodes)[key]
 	if !ok {
 		n = newNode(key)
@@ -75,12 +149,13 @@ func getNode(nodes *map[string]*node, key string) *node {
 	return n
 }
 
-func newNode(key string) *node {
-	return &node{key, make([]*node, 0), make([]*node, 0)}
+func newNode(key rune) *node {
+	return &node{key, make([]*node, 0), make([]*node, 0), 60 + int(key) - 64}
 }
 
 type node struct {
-	key string
+	key rune
 	parent []*node
 	child []*node
+	duration int
 }
