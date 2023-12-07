52f17c81b93b30b40498113c4fe82792977754f6
Steven Hyland <2knowindeed@gmail.com>
2023-12-06 21:57:44 -0500

    tweak for array, spent way too long on the p2 loop

--- 2023/code/Day4.cs
+++ 2023/code/Day4.cs
@@ -7,14 +7,11 @@ public class Day4 : IDaySolver {
 
         var p1points = 0;
         var p2cards = 0;
-        var numCards = new Dictionary<int, int>();
-        for (int i = 1; i <= 190; i++) {
-            numCards.Add(i, 1);
-        }
+        var numCards = lines.Select(l => 1).ToArray(); 
 
         foreach (var line in lines) {
             var ints = Common.GetIntsFromString(line).ToList();
-            var card = ints[0];
+            var card = ints[0] - 1;
             var winning = ints[1..11];
             var having = ints[11..];
             var hits = having.Sum(x => winning.Contains(x) ? 1 : 0);
